ff45877e6ab2ea2edf1fe56135da218e392b54a8
Florent Lepretre <florent.lepretre@zenika.com>
2016-12-09 11:30:56 +0100

    init tests

---
+++ js/jest/__tests__/kebab.spec.js
@@ -0,0 +1,11 @@
+jest.dontMock('../kebab.js');
+
+const Kebab = require('../kebab.js');
+
+describe('kebab', () => {
+  it('is veggy', () => {
+    const kebab = new Kebab();
+
+    expect
+  });
+});
