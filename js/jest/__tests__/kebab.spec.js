41ec1e1b6bb5cab864b00b26515f2b95de5e9874
Florent Lepretre <florent.lepretre@zenika.com>
2016-12-09 12:15:40 +0100

    kebabception + double

--- js/jest/__tests__/kebab.spec.js
+++ js/jest/__tests__/kebab.spec.js
@@ -4,19 +4,19 @@ const Kebab = require('../kebab.js');
 
 describe('kebab', () => {
   it('is veggy', () => {
-    const kebab = new Kebab([{name: 'salad', type: 'veggy'}]);
+    const kebab = new Kebab(undefined, [{name: 'salad', type: 'veggy'}]);
 
     expect(kebab.isVegetarian()).toBe(true);
   });
 
   it('is not veggy', () => {
-    const kebab = new Kebab([{name: 'steak', type: 'carny'}]);
+    const kebab = new Kebab(undefined, [{name: 'steak', type: 'carny'}]);
 
     expect(kebab.isVegetarian()).toBe(false);
   });
 
   it('is pescetarian', () => {
-    const kebab = new Kebab([{name: 'shrimps', type: 'pescetarian'}]);
+    const kebab = new Kebab(undefined, [{name: 'shrimps', type: 'pescetarian'}]);
 
     expect(kebab.isPescetarian()).toBe(true);
   });
--- js/jest/kebab.js
+++ js/jest/kebab.js
@@ -1,8 +1,9 @@
 const _ = require('lodash');
 
 class Kebab {
-	constructor(ingredients){
-		this.ingredients = ingredients;
+	constructor(kebab = {ingredients: []}, ingredients = [], double = false){
+		this.ingredients = kebab.ingredients.concat(ingredients);
+    this.double = double;
 	}
 
 	isVegetarian() {
@@ -11,6 +12,17 @@ class Kebab {
 
 	isPescetarian() {
 		return _.every(this.ingredients, {type: 'pescetarian'});
+	}
+
+	double() {
+		return _.reduce(this.ingredients, (e, accumulator) => {
+      if (this.double) {
+			  accumulator.push(e);
+      }
+      accumulator.push(e);
+
+      return accumulator;
+    }, []);
 	}
 }
 
